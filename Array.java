a50b7d291ba70233bf519595d274796a2056e12b
e-koks57 <tellomyk5@gmail.com>
2024-10-07 20:03:52 +0800

    Created Array.java

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Array{
+  
+  public static void main(String[]args){
+    
+    int array [] = {1,3,2};
+    
+   for(int i = 0; i < array.length; i++){
+     System.out.println(array[i]);
+     }
+    }
+  
+ 
+  }
